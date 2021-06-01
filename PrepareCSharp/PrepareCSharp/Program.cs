2e95c985616ebec59867f2ae3e72fe49f24fc5f2
CoMong2 <ghroh0915@gmail.com>
2021-06-01 22:03:39 +0900

    init commit

---
+++ PrepareCSharp/PrepareCSharp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PrepareCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
